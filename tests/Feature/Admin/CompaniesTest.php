abaaac858cb634d6b875a735b753f0129edf2d79
mirmovsum-abasov <abasov_m97@mail.ru>
2021-07-18 15:24:41 +0400

    PHPUnit Testing CompanyTest

---
+++ tests/Feature/Admin/CompaniesTest.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Tests\Feature\Admin;
+
+use App\Models\Company;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\Feature\UserTest;
+use Tests\TestCase;
+
+class CompaniesTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    use WithFaker;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->actingAs(User::find(1), 'web');
+    }
+
+    public function testIndex()
+    {
+        Company::factory()->count(5)->create();
+        $this->get("/admin/companies")->assertStatus(200);
+    }
+
+    public function testCreate()
+    {
+        $this->get('/admin/companies/create')->assertStatus(200);
+    }
+
+    public function testStore()
+    {
+        $params = $this->validParams();
+
+        $this->post('/admin/companies', $params)
+            ->assertStatus(302);
+
+        $this->assertDatabaseHas('companies', $params);
+    }
+
+    public function testStoreFail()
+    {
+        $this->post('/admin/companies')
+            ->assertStatus(302)
+            ->assertSessionHas('errors');
+    }
+
+    public function testUpdate()
+    {
+        $company = Company::factory()->create();
+
+        $this->put('/admin/companies/' . $company->id, $this->validParams())->assertStatus(302);
+
+        $this->assertDatabaseHas('companies', ['id' => $company->id]);
+    }
+
+    public function testDelete()
+    {
+        $company = Company::factory()->create();
+
+        $this->delete("/admin/companies/{$company->id}")->assertStatus(302);
+    }
+
+    private function validParams($overrides = [])
+    {
+        return array_merge([
+            'name' => $this->faker->company,
+            'email' => $this->faker->email,
+            'website' => $this->faker->domainName,
+        ], $overrides);
+    }
+}
